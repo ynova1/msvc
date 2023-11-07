c23175707bd9951c796026b711076cbdbce73027
Yeferson Nova <yefrson_nova@outlook.com>
2023-11-07 04:08:00 -0500

    microservicio de cursos funcional

---
+++ msvc-cursos/src/main/java/com/bdb/msvc/cursos/MsvcCursosApplication.java
@@ -0,0 +1,13 @@
+package com.bdb.msvc.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCursosApplication.class, args);
+	}
+
+}
---
+++ msvc-cursos/src/main/java/com/bdb/msvc/cursos/models/entity/Curso.java
@@ -0,0 +1,35 @@
+package com.bdb.msvc.cursos.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
