368aa3ce3fec0a585d3db28e9141bbeb487cf47e
Yeferson Nova <yefrson_nova@outlook.com>
2023-11-07 04:05:41 -0500

    microservicio de usuarios funcional

---
+++ msvc-usuarios/src/main/java/com/bdb/msvc/usuarios/MsvcApplication.java
@@ -0,0 +1,13 @@
+package com.bdb.msvc.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcApplication.class, args);
+	}
+
+}
