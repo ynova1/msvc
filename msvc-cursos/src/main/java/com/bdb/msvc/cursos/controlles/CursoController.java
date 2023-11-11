ed1390fc12ef63e69e97120109e7fc7bdb613bc0
Yeferson Nova <yefrson_nova@outlook.com>
2023-11-11 08:37:44 -0500

    se agregan validaciones

--- msvc-cursos/src/main/java/com/bdb/msvc/cursos/controlles/CursoController.java
+++ msvc-cursos/src/main/java/com/bdb/msvc/cursos/controlles/CursoController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
